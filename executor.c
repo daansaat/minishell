667a75b67772b1a0a80e99c63e60cd369292c279
Daan Saat <daansaat@Daans-MacBook-Air.local>
2022-07-06 13:37:54 +0200

    second

--- executor.c
+++ executor.c
@@ -3,16 +3,17 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-typedef struct	s_file
+typedef struct	s_fd
 {
 	int	tmpin;
 	int	tmpout;
 	int	in;
 	int	out;
-}				t_file;
+}				t_fd;
 
 typedef struct	s_scmd
 {
+	int		pid;
 	char	*args[3];
 }				t_scmd;
 
@@ -21,10 +22,10 @@ typedef struct	s_cmd
 	int		num_simple_cmds;
 	char	*infile;
 	char	*outfile;
-	t_scmd	*scmd[1];
+	t_scmd	*scmd;
 }				t_cmd;
 
-void	set_pipe(t_file *fd)
+void	set_pipe(t_fd *fd)
 {
 	int	fdpipe[2];
 
@@ -37,53 +38,58 @@ void	set_pipe(t_file *fd)
 	fd->out = dup(fdpipe[1]); // fdpipe[1] - write - output
 }
 
-void	do_execute(t_list *cmd)
+void	do_execute(t_scmd *scmd)
 {
-	int	pid;
-	int	status;
+	// int	pid;
+	// int	status;
 
-	pid = fork();
-	if (pid == -1)
+	scmd->pid = fork();
+	if (scmd->pid == -1)
 	{
 		perror("fork()");
 		exit(EXIT_FAILURE);
 	}
-	if (pid == 0)
+	if (scmd->pid == 0)
 	{
-		execvp(cmd->scmd[i].args[0], cmd->scmd[i].args);
+		execvp(scmd->args[0], scmd->args);
 		perror("execvp()");
 		exit(1);
 	}
-	if (waitpid(0, &status, 0) == -1)
-	{
-		perror("waitpid()");
-		exit(EXIT_FAILURE);
-	}
-	if (WIFEXITED(status))
-		if (WEXITSTATUS(status) == 1)
-			exit(EXIT_FAILURE);
+	// if (waitpid(0, &status, 0) == -1)
+	// {
+	// 	perror("waitpid()");
+	// 	exit(EXIT_FAILURE);
+	// }
+	// if (WIFEXITED(status))
+	// 	if (WEXITSTATUS(status) == 1)
+	// 		exit(EXIT_FAILURE);
 }
 
-int	check_redirection_out(t_list *cmd, t_file *fd)
+int	check_redirection_out(t_cmd *cmd, t_fd *fd)
 {
 	if (cmd->outfile)
+	{
 		return (open(cmd->outfile, O_WRONLY));
+	}
 	else
 		return (dup(fd->tmpout));
 }
 
-int	check_redirection_in(t_list *cmd, t_file *fd)
+int	check_redirection_in(t_cmd *cmd, t_fd *fd)
 {
 	if (cmd->infile)
+	{
+		printf("yes\n");
 		return (open(cmd->infile, O_RDONLY));
+	}
 	else
 		return (dup(fd->tmpin));
 }
 
-void	execute(t_list *cmd)
+void	execute(t_cmd *cmd)
 {
-	t_file	fd;
-	int i;
+	t_fd	fd;
+	int 	i;
 
 	fd.tmpin = dup(STDIN_FILENO);
 	fd.tmpout = dup(STDOUT_FILENO);
@@ -91,31 +97,46 @@ void	execute(t_list *cmd)
 	i = 0;
 	while (i <= cmd->num_simple_cmds - 1)
 	{
-		dup2(fd.in, STDIN_FILENO);
+		if (dup2(fd.in, STDIN_FILENO) == -1)
+			perror("dup2()");
 		close(fd.in);
 		if (i == cmd->num_simple_cmds - 1)
 			fd.out = check_redirection_out(cmd, &fd);
 		else
 			set_pipe(&fd);
-		dup2(fd.out, STDOUT_FILENO);
+		if (dup2(fd.out, STDOUT_FILENO) == -1)
+			perror("dup2()");
 		close(fd.out);
-		do_execute(cmd);
+		do_execute(&cmd->scmd[i]);
 		i++;
 	}
-	dup2(fd.tmpin, STDIN_FILENO);
-	dup2(fd.tmpout, STDOUT_FILENO);
+	if (dup2(fd.tmpin, STDIN_FILENO) == -1)
+		perror("dup2()");
+	if (dup2(fd.tmpout, STDOUT_FILENO) == -1)
+		perror("dup2()");
 	close(fd.tmpin);
 	close(fd.tmpout);
+	waitpid(cmd->scmd[i - 1].pid, NULL, 0);
+	printf("hello\n");
 }
 
 int	main(void)
 {
-	t_list	cmd;
+	t_cmd	cmd;
 
-	cmd.num_simple_cmds = 1;
+	cmd.num_simple_cmds = 2;
+	cmd.scmd = malloc(sizeof(t_scmd) * 2);
 	cmd.scmd[0].args[0] = "ls";
-	cmd.scmd[0].args[1] = "-al";
+	cmd.scmd[0].args[1] = "-la";
 	cmd.scmd[0].args[2] = NULL;
+	// cmd.scmd[0].args[0] = "echo";
+	// cmd.scmd[0].args[1] = "hello";
+	// cmd.scmd[0].args[2] = NULL;
+	// cmd.scmd[0].args[2] = NULL;
+	cmd.scmd[1].args[0] = "wc";
+	cmd.scmd[1].args[1] = "-c";
+	cmd.scmd[1].args[1] = NULL;
+	// cmd.infile = "~/Documents/codam/minishell/hello";
 	execute(&cmd);
 	return (0);
 }
